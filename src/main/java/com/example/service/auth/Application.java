3cd464434aa7a449c6f24bf1b25b423ea141831c
kc-mitchell <steve.mitchell@mobilecapitalgroup.com>
2019-04-07 16:09:44 -0500

    Removed @EnableOAuth2Sso

--- src/main/java/com/example/service/auth/Application.java
+++ src/main/java/com/example/service/auth/Application.java
@@ -2,12 +2,10 @@ package com.example.service.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-@EnableOAuth2Sso
 @EnableJpaAuditing
 @SpringBootApplication
 @EnableAuthorizationServer
