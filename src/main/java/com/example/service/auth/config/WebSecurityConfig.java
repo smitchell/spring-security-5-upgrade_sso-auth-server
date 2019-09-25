0bc917b4e3630684447bbda7ca36e8e39d301093
smitchell1 <steve@platformconsulting.io>
2019-09-25 09:07:44 -0500

    Removed session property

--- src/main/java/com/example/service/auth/config/WebSecurityConfig.java
+++ src/main/java/com/example/service/auth/config/WebSecurityConfig.java
@@ -63,7 +63,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-
     // @formatter:off
     http
         .requestMatchers()
